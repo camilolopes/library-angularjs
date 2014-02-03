71251df468dca9cacd82bee506f1093e2b29cf1a
Camilo Lopes <camilo@camilolopes.com.br>
2014-02-03 17:27:16 -0200

    added panel list book form

--- src/main/webapp/app/scripts/controllers/ebookRegister.js
+++ src/main/webapp/app/scripts/controllers/ebookRegister.js
@@ -4,10 +4,11 @@ angular.module('webappApp')
   .controller('EbookregisterCtrl',['$scope','ebookService', function ($scope,ebookService) {
     
     $scope.book = new ebookService();
-    $scope.listBooks;
+    $scope.listBooks = ebookService.list();
     $scope.save = function(){	
     	$scope.book.$save(function(){
     		$scope.book = new ebookService();
+    		$scope.listBooks = ebookService.list();
     	});    	
     }
   }]);
