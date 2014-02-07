eb060018c5837cb9405691208a6f44f08604d29d
Camilo Lopes <camilo@camilolopes.com.br>
2014-02-07 16:35:57 -0200

    dashboard done

--- src/main/webapp/app/scripts/app.js
+++ src/main/webapp/app/scripts/app.js
@@ -10,7 +10,7 @@ angular.module('webappApp', [
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'EbookregisterCtrl'
       })
       .when('/ebookRegister', {
         templateUrl: 'views/ebookregister.html',
